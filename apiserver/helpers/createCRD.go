55281a4e67cbd4b90a5dbbc23efba7854e686360
dgkanatsios <dgkanatsios@outlook.com>
2018-07-31 14:17:52 +0300

    fix

--- apiserver/helpers/createCRD.go
+++ apiserver/helpers/createCRD.go
@@ -14,7 +14,7 @@ func CreateDedicatedGameServerCRD(dgsInfo DedicatedGameServerInfo) (string, erro
 
 	for _, portInfo := range dgsInfo.Ports {
 		//get a random port
-		hostport, err := shared.GetNewPort()
+		hostport, err := shared.GetNewPort(dgsInfo.Name)
 		if err != nil {
 			return "", err
 		}

922af0bdf0ab12d6e15413e1dd4b0e1a82b77736
Dimitris-Ilias Gkanatsios <dgkanatsios@outlook.com>
2018-09-23 14:21:11 +0300

    Fixes (#49)
    * various
    
    * update on simplenodejsudp demo

--- apiserver/webhookserver/run.go
+++ apiserver/webhookserver/run.go
@@ -169,7 +169,7 @@ func (whsvr *WebhookServer) serve(w http.ResponseWriter, r *http.Request) {
 		log.Errorf("Can't encode response: %v", err)
 		http.Error(w, fmt.Sprintf("could not encode response: %v", err), http.StatusInternalServerError)
 	}
-	log.Infof("Ready to write reponse ...")
+
 	if _, err := w.Write(resp); err != nil {
 		log.Errorf("Can't write response: %v", err)
 		http.Error(w, fmt.Sprintf("could not write response: %v", err), http.StatusInternalServerError)

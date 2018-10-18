107d4cf5ea33a5654c19c2569faba0d5205705ea
Dimitris-Ilias Gkanatsios <dgkanatsios@outlook.com>
2018-10-18 10:10:14 +0300

    various fixes

--- artifacts/examples/openarena/autoscalerdemo/autoscaler.sh
+++ artifacts/examples/openarena/autoscalerdemo/autoscaler.sh
@@ -5,18 +5,18 @@ kubectl create -f dedicatedgameservercollection.yaml
 # demo scale out
 
 # get DGS names
-dgs=`kubectl get dgs -l DedicatedGameServerCollectionName=openarena-collection-example | cut -d ' ' -f 1 | sed 1,1d`
+dgs=`kubectl get dgs -l DedicatedGameServerCollectionName=openarena | cut -d ' ' -f 1 | sed 1,1d`
 # update DGS.Spec.ActivePlayers
 kubectl patch dgs $dgs -p '[{ "op": "replace", "path": "/status/activePlayers", "value": 9 },]' --type='json'
 # update DGS.Labels[ActivePlayers]
 kubectl label dgs $dgs ActivePlayers=9 --overwrite
 
 # edit the last scaling operation datetime
-kubectl edit svc openarena-collection-example
+kubectl edit dgsc openarena
 
 # demo scale in
 # get DGS names - again
-dgs=`kubectl get dgs -l DedicatedGameServerCollectionName=openarena-collection-example | cut -d ' ' -f 1 | sed 1,1d`
+dgs=`kubectl get dgs -l DedicatedGameServerCollectionName=openarena | cut -d ' ' -f 1 | sed 1,1d`
 # update DGS.Spec.ActivePlayers
 kubectl patch dgs $dgs -p '[{ "op": "replace", "path": "/status/activePlayers", "value": 5 },]' --type='json'
 # update DGS.Labels[ActivePlayers]
--- artifacts/examples/simplenodejsudp/modify_active_players.sh
+++ artifacts/examples/simplenodejsudp/modify_active_players.sh
@@ -5,7 +5,7 @@ kubectl create -f dedicatedgameservercollection.yaml
 # demo scale out
 
 # get DGS names
-dgs=`kubectl get dgs -l DedicatedGameServerCollectionName=simplenodejsudp-collection-example | cut -d ' ' -f 1 | sed 1,1d`
+dgs=`kubectl get dgs -l DedicatedGameServerCollectionName=simplenodejsudp | cut -d ' ' -f 1 | sed 1,1d`
 # update DGS.Spec.ActivePlayers
 kubectl patch dgs $dgs -p '[{ "op": "replace", "path": "/status/activePlayers", "value": 5 },]' --type='json'
 # update DGS.Labels[ActivePlayers]
